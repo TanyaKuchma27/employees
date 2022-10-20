2cc87a0d8871410067a16ec7902e820bc92a0a5b
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-10-20 19:34:46 +0400

    add Birthday

--- src/components/Birthday/Birthday.jsx
+++ src/components/Birthday/Birthday.jsx
@@ -5,15 +5,13 @@ import { Title, List, Text } from './Birthday.styled';
 export const Birthday = () => {
     const birthday = useSelector(getBirthday.selectAll);
 
-    console.log(birthday)
-
     return (
         <>
             <Title>Employees birthday</Title>
             {(birthday.length > 0) ? (
                 <ul>
                     {birthday.map(item =>
-                        <List key={item.id}>{item.lastName} {item.firstName} - { item.dob}</List>)}
+                        <List key={item.id}>{item.lastName} {item.firstName} - {item.dob}</List>)}
                 </ul>
             ) : (<Text>Employees List is empty</Text>)}
         </>
